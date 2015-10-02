c450e523a4cb223b2fc70dd00eabc7b42bc05cd6
change <844631704s@gmail.com>
2015-10-02 20:38:43 +0800

    位置，速度调整

--- src/solar/SolarFrame.java
+++ src/solar/SolarFrame.java
@@ -10,15 +10,18 @@ import util.MyFrame;
 public class SolarFrame extends MyFrame{
 	Image bg=GameUtil.getImage("images/bg.jpg");
 	Star sun=new Star("images/sun.jpg",Constant.GAME_WIDTH/2,Constant.GAME_HEIGHT/2);
-	Planet earth=new Planet(sun,"images/earth.jpg",200,100,0.05);
-	Planet Jupiter=new Planet(sun,"images/Jupiter.jpg",200,100,0.06);
-	Planet Mars=new Planet(sun,"images/Mars.jpg",200,100,0.07);
-	Planet Mercury=new Planet(sun,"images/Mercury.jpg",200,100,0.08);
-	Planet moon=new Planet(sun,"images/moon.jpg",200,100,0.09);
-	Planet Neptune=new Planet(sun,"images/Neptune.jpg",200,100,0.10);
-	Planet Saturn=new Planet(sun,"images/Saturn.jpg",200,100,0.11);
-	Planet Uranus=new Planet(sun,"images/Uranus.jpg",200,100,0.12);
-	Planet Venus=new Planet(sun,"images/Venus.jpg",200,100,0.13);
+	Planet Mercury=new Planet(sun,"images/Mercury.jpg",100,50,0.13);//水星
+	Planet Venus=new Planet(sun,"images/Venus.jpg",150,100,0.12);//金星
+	Planet earth=new Planet(sun,"images/earth.jpg",200,150,0.11);//地
+	Planet moon=new Planet(earth,"images/moon.jpg",50,30,0.15);
+	Planet Mars=new Planet(sun,"images/Mars.jpg",250,200,0.1);//火星
+	Planet Jupiter=new Planet(sun,"images/Jupiter.jpg",260,210,0.09);//木星
+	Planet Saturn=new Planet(sun,"images/Saturn.jpg",270,220,0.07);//土星
+	Planet Uranus=new Planet(sun,"images/Uranus.jpg",280,230,0.03);//天王星
+	Planet Neptune=new Planet(sun,"images/Neptune.jpg",300,250,0.01);//海王星
+	
+	
+	
 	/*
 	private double x=100;
 	private double y=100;*/
